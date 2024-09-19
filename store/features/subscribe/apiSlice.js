0d63a53aa73e4158b24bb75aba4a6c8b5d5ef0e2
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-19 16:36:03 +0630

    refine news letter request api

--- store/features/subscribe/apiSlice.js
+++ store/features/subscribe/apiSlice.js
@@ -4,49 +4,18 @@ import { toast } from "react-toastify";
 export const subscribeApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     sendEmail: builder.mutation({
-      query: ({ data }) => {
+      query: ({ data, handler }) => {
         return {
           url: `/newsletter-request?lang_code=en`,
           method: "POST",
           body: data,
         };
       },
-      async onQueryStarted(info, { queryFulfilled }) {
-        const id = toast.loading("Please Wait...", {
+      async onQueryStarted(info, { queryFulfilled, dispatch }) {
+        toast.loading("Please Wait ...", {
           position: "top-right",
           closeButton: true,
         });
-        try {
-          const { data, meta } = await queryFulfilled;
-          if (meta?.response?.status === 200) {
-            info.handler("");
-          }
-
-          if (data?.message) {
-            toast.update(id, {
-              render: `${data?.message}`,
-              type: "success",
-              isLoading: false,
-              autoClose: 2000,
-            });
-          }
-        } catch ({ error }) {
-          if (error?.data?.message) {
-            toast.update(id, {
-              render: `${error?.data?.message}`,
-              type: "error",
-              isLoading: false,
-              autoClose: 2000,
-            });
-          } else {
-            toast.update(id, {
-              render: `Error occurred!`,
-              type: "error",
-              isLoading: false,
-              autoClose: 2000,
-            });
-          }
-        }
       },
     }),
   }),
