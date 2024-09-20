29be9e0ff306b45c10b15d89e786b4925e846b5f
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-20 13:35:02 +0630

    update newsletter request function

--- store/features/subscribe/apiSlice.js
+++ store/features/subscribe/apiSlice.js
@@ -12,10 +12,41 @@ export const subscribeApi = apiSlice.injectEndpoints({
         };
       },
       async onQueryStarted(info, { queryFulfilled, dispatch }) {
-        toast.loading("Please Wait ...", {
+        const id = toast.loading("Please Wait...", {
           position: "top-right",
           closeButton: true,
         });
+        try {
+          const { data, meta } = await queryFulfilled;
+          if (meta?.response?.status === 200) {
+            info.handler("");
+          }
+
+          if (data?.message) {
+            toast.update(id, {
+              render: `${data?.message}`,
+              type: "success",
+              isLoading: false,
+              autoClose: 2000,
+            });
+          }
+        } catch ({ error }) {
+          if (error?.data?.message) {
+            toast.update(id, {
+              render: `${error?.data?.message}`,
+              type: "error",
+              isLoading: false,
+              autoClose: 2000,
+            });
+          } else {
+            toast.update(id, {
+              render: `Error occurred!`,
+              type: "error",
+              isLoading: false,
+              autoClose: 2000,
+            });
+          }
+        }
       },
     }),
   }),
