9e18b44117042cd1191b2bcf15623317dd0899cc
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-21 14:29:51 +0630

    add categories slide

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     BASE_URL: "http://localhost:8000/", // You must have a base API URL
   },
+  images: {
+    domains: ["plus.unsplash.com", "images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
