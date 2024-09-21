80c39c1809a1ae0157e7bd4fa43349e8ff651028
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-21 15:04:38 +0630

    change the whole theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +8,16 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          black: "#0D0127",
+          gray: "#616161",
+          blue: "#45F882", //replace with this #0E6DE8
+          yellow: "#45F882", //replace with this #FFB321
+          border: "#1a1a1a",
+          "border-secondary": "#D1E5FF",
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;
