2c9a29d8993e4dcdb97dc6a04091e02478a49c2a
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-22 10:33:03 +0630

    integrate reigister api

--- store/features/auth/apiSlice.js
+++ store/features/auth/apiSlice.js
@@ -7,12 +7,69 @@ export const authApi = apiSlice.injectEndpoints({
       query: () => ({
         url: `user-logout`,
       }),
-      async onQueryStarted(info, { queryFulfilled, dispatch }) {
-        const id = toast.loading("Please Wait...", {
+      async onQueryStarted(info, { dispatch }) {
+        toast.loading("Please Wait...", {
           position: "top-right",
           closeButton: true,
         });
         dispatch(userLoggedOut());
+      },
+    }),
+    userRegister: builder.mutation({
+      query: (data) => {
+        const bodyFormData = new FormData();
+        Object.entries({
+          firstName: data.firstName,
+          lastName: data.lastName,
+          name: data.userName,
+          email: data.email,
+          password: data.password,
+          password_confirmation: data.password_confirmation,
+          agree: data.agree,
+        }).map(([key, value]) => {
+          bodyFormData.append(`${key}`, value);
+        });
+        return {
+          url: "store-register",
+          method: "POST",
+          body: bodyFormData,
+          formData: true,
+        };
+      },
+      async onQueryStarted(info, { queryFulfilled }) {
+        const id = toast.loading("Please Wait...", {
+          position: "top-right",
+          closeButton: true,
+        });
+        try {
+          const { data } = await queryFulfilled;
+          if (data.message.includes("user_validation.Register successfully")) {
+            toast.update(id, {
+              render: `Account created successfully.`,
+              type: "success",
+              isLoading: false,
+              autoClose: 2000,
+            });
+          } else if (data.message) {
+            toast.update(id, {
+              render: data.message,
+              type: "error",
+              isLoading: false,
+              // autoClose: 2000,
+            });
+          }
+        } catch ({ error }) {
+          console.log(
+            "Improved Spoon Error    !!!!!!!!!!!!!!!!!!!!!!!!!  " +
+              error.data.message
+          );
+          toast.update(id, {
+            render: error.data.message,
+            type: "error",
+            isLoading: false,
+            autoClose: 2000,
+          });
+        }
       },
     }),
   }),
